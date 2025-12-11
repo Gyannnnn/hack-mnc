18eeb844b1e4d5331f4c46b95f21f1a62e04912b
Gyan <hi.gyanaranjanpatra@gmail.com>
2025-12-11 11:55:30 +0530

    fix;cve problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -157,7 +157,7 @@ export default function RootLayout({
             <Providers>
               <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
                 <Navbar />
-                <main className="pt-20 min-h-screen flex items-center justify-center w-full sm:px-10">
+                <main className="pt-10 min-h-screen flex items-center justify-center w-full sm:px-10">
                   {children}
                 </main>
                 <Toaster position="top-center" />
--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,50 @@
 import type { NextConfig } from "next";
 
-// next.config.js
-const NextConfig = {
+const nextConfig: NextConfig = {
   images: {
-    domains: ['www.gstatic.com','logopoppin.com','imgs.search.brave.com','msblogs.thesourcemediaassets.com','veerpreps.s3.ap-south-1.amazonaws.com','www.paypalobjects.com','lh3.googleusercontent.com',"logo.clearbit.com" ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "veerpreps.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.gstatic.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "logopoppin.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "imgs.search.brave.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "msblogs.thesourcemediaassets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.paypalobjects.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "logo.clearbit.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
-export default NextConfig;
-
-
-
+export default nextConfig;
