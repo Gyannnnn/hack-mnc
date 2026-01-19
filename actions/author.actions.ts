340084d2655e9202484df4879d93ba3d15d7c00b
Gyan <hi.gyanaranjanpatra@gmail.com>
2026-01-19 12:06:15 +0530

    backend url

--- actions/author.actions.ts
+++ actions/author.actions.ts
@@ -4,7 +4,7 @@ import { AuthorResponse } from "@/types/author.types";
 
 export const GetAllAuthors = async (): Promise<AuthorResponse | null> => {
     try {
-        const response = await axios.get("http://localhost:8080/api/v1/author/all");
+        const response = await axios.get("https://api.hackmnc.com/api/v1/author/all");
         if (response.status === 200) {
             return response.data;
         }
